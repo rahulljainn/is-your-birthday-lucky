e05cb92bebb2f963924a76d00db397d6a6745de9
Rahul Jain <rahulllljainnnn@gmail.com>
2022-08-19 16:26:08 +0530

    update

---
+++ app.js
@@ -0,0 +1,30 @@
+const dateofbirth = document.querySelector("#dateOfBirth");
+const luckynumber = document.querySelector("#luckyNumber");
+const checknumberbutton = document.querySelector("#checkNumber");
+const resultbox= document.querySelector("#resultbox")
+
+    function compareValues(sum,luckynumber){
+        if(sum%luckynumber===0){
+            resultbox.innerText=("BINGO!! you are lucky😍");
+         } else{
+            resultbox.innerText=("SORRY you are unlucky😒");
+            }
+        }
+    
+
+
+function confirmbirthdaylucky(){
+    const dob = dateofbirth.value;
+    const sum = calculatesum(dob);
+    compareValues(sum,luckynumber.value)
+
+}
+function calculatesum(dob){
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(let i=0;i<dob.length;i++){
+        sum=sum+ Number(dob.charAt(i));
+    }
+    return sum;
+}
+    checknumberbutton.addEventListener("click",confirmbirthdaylucky)
